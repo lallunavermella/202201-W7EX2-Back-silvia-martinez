c9efb280c56d4af2ac812fd53b35e74eb3f07762
Silvia Martinez <sil@silvia.com>
2022-02-22 14:02:17 +0100

    Fixed userController

--- src/server/controllers/userControllers.js
+++ src/server/controllers/userControllers.js
@@ -5,12 +5,14 @@ const User = require("../../database/models/User");
 const loginUser = async (req, res, next) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
+
   if (!user) {
     const error = new Error("User not Found");
     error.code = 401;
     next(error);
   }
   const isRigthPassword = await bcrypt.compare(password, user.password);
+
   if (!isRigthPassword) {
     const error = new Error("Incorrect password");
     error.code = 401;
